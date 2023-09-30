4eb2441c905af6d306babd1f70a29a7e888303d5
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2023-09-30 14:05:18 +1000

    Update Lab 8

---
+++ Lab 8/Lab8PClient.py
@@ -0,0 +1,33 @@
+import socket
+import argparse
+
+
+def udp_client_listener(host: str, port: int, buffer_size: int = 1024) -> None:
+    client_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        msg = input("Enter message to send to server (or 'exit' to end): ")
+
+        if msg.lower() == 'exit':
+            break
+
+        client_soc.sendto(f"TNE20003:{msg}".encode('ascii'), (host, port))
+
+        data, address = client_soc.recvfrom(buffer_size)
+        print(f"Received from server {address}: {data.decode('ascii')}")
+    client_soc.close()
+
+
+def main() -> None:
+    arg_parser = argparse.ArgumentParser("Client-side UDP for Lab 8P")
+    arg_parser.add_argument(
+        "-host", help="Host of the server, default is localhost", default="127.0.0.1", type=str)
+    arg_parser.add_argument(
+        "-port", help="Port of the server, default is 4231", default=4231, type=int)
+    arg_parser.add_argument(
+        "-buffer_size", help="Buffer size of the packet, default is 1024", default=1024, type=int)
+    args = arg_parser.parse_args()
+    udp_client_listener(args.host, args.port, args.buffer_size)
+
+
+if __name__ == "__main__":
+    main()
